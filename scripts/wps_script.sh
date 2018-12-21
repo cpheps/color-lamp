89bf000c8911672e171a2b24c6fc53b87b288d8b
Corbin Phelps <corbin.phelps@bluemedora.com>
2018-12-21 11:59:36 -0500

    Updated wifi script search

--- scripts/wps_script.sh
+++ scripts/wps_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if ! ifconfig wlan0 | grep -q "inet addr:" ; then
+if ! ifconfig wlan0 | grep -q "inet" ; then
     wpa_cli scan_results | grep WPS | sort -r -k3 | tail -1 | awk '{print $1;}' > /tmp/wifi
     read ssid < /tmp/wifi
     wpa_cli -i wlan0 wps_pbc $ssid
