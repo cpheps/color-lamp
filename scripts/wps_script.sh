49207636af0db78255a2c1e2fe55f9b5f5912e5a
Corbin Phelps <corbin.phelps@bluemedora.com>
2018-12-21 10:54:08 -0500

    Fixed wifi script

--- scripts/wps_script.sh
+++ scripts/wps_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if ! ifconfig wlan0 | grep -q "inet addr:" ; then
-    wpa_cli scan_results | grep WPS | sort -r -k3 | tail -l | awk '{print $1;}' > /tmp/wifi
+    wpa_cli scan_results | grep WPS | sort -r -k3 | tail -1 | awk '{print $1;}' > /tmp/wifi
     read ssid < /tmp/wifi
     wpa_cli -i wlan0 wps_pbc $ssid
 fi
